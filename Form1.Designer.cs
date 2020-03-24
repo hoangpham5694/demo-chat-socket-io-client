ad193ef8b04383ace5676e1474d43fad0ad4aee0
Your Name <hoangpham5694@gmail.com>
2020-03-24 17:08:33 +0700

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace TestSocket
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChat = new System.Windows.Forms.Button();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtLogMessage = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtRoom = new System.Windows.Forms.TextBox();
+            this.btnJoinRoom = new System.Windows.Forms.Button();
+            this.btnMyRoom = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnChat
+            // 
+            this.btnChat.Location = new System.Drawing.Point(269, 401);
+            this.btnChat.Name = "btnChat";
+            this.btnChat.Size = new System.Drawing.Size(75, 23);
+            this.btnChat.TabIndex = 0;
+            this.btnChat.Text = "Chat";
+            this.btnChat.UseVisualStyleBackColor = true;
+            this.btnChat.Click += new System.EventHandler(this.btnChat_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(86, 403);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(161, 20);
+            this.txtMessage.TabIndex = 1;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(86, 364);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(161, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtLogMessage
+            // 
+            this.txtLogMessage.Location = new System.Drawing.Point(24, 40);
+            this.txtLogMessage.Multiline = true;
+            this.txtLogMessage.Name = "txtLogMessage";
+            this.txtLogMessage.Size = new System.Drawing.Size(320, 275);
+            this.txtLogMessage.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 367);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 409);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Message:";
+            // 
+            // txtRoom
+            // 
+            this.txtRoom.Location = new System.Drawing.Point(475, 40);
+            this.txtRoom.Name = "txtRoom";
+            this.txtRoom.Size = new System.Drawing.Size(100, 20);
+            this.txtRoom.TabIndex = 6;
+            // 
+            // btnJoinRoom
+            // 
+            this.btnJoinRoom.Location = new System.Drawing.Point(603, 40);
+            this.btnJoinRoom.Name = "btnJoinRoom";
+            this.btnJoinRoom.Size = new System.Drawing.Size(75, 23);
+            this.btnJoinRoom.TabIndex = 7;
+            this.btnJoinRoom.Text = "JoinRoom";
+            this.btnJoinRoom.UseVisualStyleBackColor = true;
+            this.btnJoinRoom.Click += new System.EventHandler(this.btnJoinRoom_Click);
+            // 
+            // btnMyRoom
+            // 
+            this.btnMyRoom.Location = new System.Drawing.Point(603, 81);
+            this.btnMyRoom.Name = "btnMyRoom";
+            this.btnMyRoom.Size = new System.Drawing.Size(75, 23);
+            this.btnMyRoom.TabIndex = 8;
+            this.btnMyRoom.Text = "MyRoom";
+            this.btnMyRoom.UseVisualStyleBackColor = true;
+            this.btnMyRoom.Click += new System.EventHandler(this.btnMyRoom_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnMyRoom);
+            this.Controls.Add(this.btnJoinRoom);
+            this.Controls.Add(this.txtRoom);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLogMessage);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.btnChat);
+            this.Name = "Form1";
+            this.Text = "Message:";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChat;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtLogMessage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtRoom;
+        private System.Windows.Forms.Button btnJoinRoom;
+        private System.Windows.Forms.Button btnMyRoom;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Quobject.SocketIoClientDotNet.Client;
+
+namespace TestSocket
+{
+    public partial class Form1 : Form
+    {
+        private string room = "default";
+        public Socket socket;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+               socket = IO.Socket("http://192.168.1.44:3000/"); 
+           // socket = IO.Socket("https://cacc5f9b.ngrok.io/");
+            socket.On(Socket.EVENT_CONNECT, () =>
+            {
+              //  socket.Emit("send_message", "asd", "asddfff");
+
+            });
+            socket.On("receiver_data", (data) =>
+            {
+                //  Console.WriteLine(data);
+
+                  dynamic item = JsonConvert.DeserializeObject(data.ToString());
+                   Console.WriteLine(Convert.ToString(item.from) );
+                //item.from + ": " + item.msg + Environment.NewLine
+                txtLogMessage.Invoke((MethodInvoker)(() => txtLogMessage.AppendText(Convert.ToString(item.from) + ": " + Convert.ToString(item.msg) + Environment.NewLine)));
+             //   txtMessage.AppendText("sdfdf");
+            });
+
+        }
+
+        private void btnChat_Click(object sender, EventArgs e)
+        {
+            
+            socket.Emit("send_message", txtName.Text, txtMessage.Text, room);
+        }
+
+        private void btnJoinRoom_Click(object sender, EventArgs e)
+        {
+            socket.Emit("leave-room", room);
+            this.room = txtRoom.Text;
+            socket.Emit("room", room);
+        }
+
+        private void btnMyRoom_Click(object sender, EventArgs e)
+        {
+            socket.Emit("my-room");
+        }
+    }
+}
